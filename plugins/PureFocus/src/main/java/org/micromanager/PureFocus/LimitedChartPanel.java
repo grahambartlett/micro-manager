bc4bad8fc8e5b1dc0887f326be68db15c64a2d30
Graham Bartlett <gbartlett@prior.com>
2021-07-28 10:22:03 +0100

    Reduce imports to only those required.

--- plugins/PureFocus/src/main/java/org/micromanager/PureFocus/LimitedChartPanel.java
+++ plugins/PureFocus/src/main/java/org/micromanager/PureFocus/LimitedChartPanel.java
@@ -11,61 +11,19 @@ Licensed under the BSD license.
 
 package org.micromanager.PureFocus;
 
-import java.awt.Color;
 import java.awt.Paint;
-import java.awt.Cursor;
-import java.awt.Shape;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.geom.Ellipse2D;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JSeparator;
-import javax.swing.JSpinner;
-import javax.swing.JTextField;
-import javax.swing.JToggleButton;
-import javax.swing.SpinnerModel;
-import javax.swing.JPopupMenu;
-import javax.swing.JMenu;
-import javax.swing.MenuElement;
-import javax.swing.Timer;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.geom.Rectangle2D;
-import java.awt.geom.Point2D;
 
 import java.text.DecimalFormat;
 
-import mmcorej.CMMCore;
-import mmcorej.DeviceType;
-import net.miginfocom.swing.MigLayout;
-
-import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.axis.NumberTickUnit;
-import org.jfree.chart.LegendItem;
-import org.jfree.chart.LegendItemCollection;
-import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.Plot;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYItemRenderer;
-import org.jfree.data.RangeType;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.chart.axis.ValueAxis;
-
-import org.micromanager.Studio;
-import org.micromanager.internal.utils.WindowPositioning;
 
 
 /** Customised chart panel which prevents zoom/autorange outside 
